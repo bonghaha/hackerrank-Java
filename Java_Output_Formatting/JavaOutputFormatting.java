cdde8762c8f3b802e4ec7633d4ecd4b7f7e4ffb4
Bongha Hwang <qhdgk825@naver.com>
2017-01-23 11:08:09 +0900

    Add JavaOutputFormatting.java

---
+++ Java_Output_Formatting/JavaOutputFormatting.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+        System.out.println("================================");
+        
+        for(int i=0; i<3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            System.out.printf( "%-15s%03d %n", s1, x);  // 이 한줄이면 끝이네..
+            
+            /*
+            System.out.print(s1);
+            for(int j=0; j<(15-s1.length()); j++){
+                System.out.print(" ");
+            }
+            
+            if(x>=100) {
+                System.out.println(x);
+            } else if(x==0) {
+                System.out.print("00");
+                System.out.println(x);
+            } else {
+                System.out.print("0");
+                System.out.println(x);
+            }
+            */
+        }
+        System.out.println("================================");
+	}
+}
