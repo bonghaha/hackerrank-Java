59a48327c347fed714e4b03fe4b406a393a08cc1
Bongha Hwang <qhdgk825@naver.com>
2017-01-23 10:38:37 +0900

    Update JavaStdinAndStdoutI.java

--- Java_Stdin_and_Stdout_I/JavaStdinAndStdoutI.java
+++ Java_Stdin_and_Stdout_I/JavaStdinAndStdoutI.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class JavaStdinAndStdoutI {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
